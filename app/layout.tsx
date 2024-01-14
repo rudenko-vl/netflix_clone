68c2912e9e38cb08ca396d93b8212201a16e1d87
rudenko-vl <84930686+rudenko-vl@users.noreply.github.com>
2024-01-14 13:40:12 +0200

    start

---
+++ app/layout.tsx
@@ -0,0 +1,21 @@
+import type { Metadata } from 'next'
+// import { Inter } from 'next/font/google'
+import './globals.css'
+
+// const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Next App clone Netflix',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="uk">
+      <body>{children}</body>
+    </html>
+  )
+}
---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+const LandingPage = () => {
+  return (
+    <>
+      <div className="flex flex-col justify-center items-center pt-80 px-5 text-center gap-10">
+        {/* <Navbar/> */}
+        <h1 className="font-extrabold text-white text-5xl">Фільми, серіали й інший контент без обмежень</h1>
+        <h4 className="font-sm text-white text-3xl">Дивіться будь-де. Скасувати підписку можна будь-коли.</h4>
+      </div>
+    </>
+  );
+};
+
+export default LandingPage;
